30a7cb27f2ad94bcdbce99c71d4191d0f25fc624
drewfus <drewfus.d@gmail.com>
2026-09-08 17:45:00 +1000

    Release v1.0.9

--- include/config.h
+++ include/config.h
@@ -40,7 +40,7 @@
 // =============================================================================
 // Firmware Version & Over-The-Air (OTA) Configuration
 // =============================================================================
-#define FIRMWARE_VERSION              "1.0.8"
+#define FIRMWARE_VERSION              "1.0.9"
 
 // Duration (in seconds) the clock listens for local wireless updates on boot / reset
 #define OTA_WINDOW_TIMEOUT_SEC        30
